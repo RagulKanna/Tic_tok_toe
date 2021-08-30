a66a45c06781751732921c3a162f3629b1a3e741
RagulKanna <ragulragi619@gmail.com>
2021-08-30 09:05:29 +0530

    Added:Initializing tictactoe board

---
+++ src/com/workshop/Tiktoktoe.java
@@ -0,0 +1,21 @@
+package com.workshop;
+
+public class Tiktoktoe {
+    public void initializeboard(){
+        char[] b=new char[10];
+        b[0]='';
+        b[1]='';
+        b[2]='';
+        b[3]='';
+        b[4]='';
+        b[5]='';
+        b[6]='';
+        b[7]='';
+        b[8]='';
+        b[9]='';
+    }
+    public static void main(String[] args) {
+        initializeboard();
+    }
+
+}
