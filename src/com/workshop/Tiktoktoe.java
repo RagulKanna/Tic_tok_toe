42665dd3222a492dcb2022ba5367ede436db7e59
RagulKanna <ragulragi619@gmail.com>
2021-08-30 10:03:59 +0530

    Added:To display board

--- src/com/workshop/Tiktoktoe.java
+++ src/com/workshop/Tiktoktoe.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Tiktoktoe {
     public static char ip;
+    public static char[] b=new char[10];
     public static Scanner sc=new Scanner(System.in);
     public static void initializeboard(){
-        char[] b=new char[10];
         b[0]=' ';
         b[1]=' ';
         b[2]=' ';
@@ -22,16 +22,29 @@ public class Tiktoktoe {
         System.out.println("\nEnter the 'X' or 'O' what you needed:");
         ip = sc.next().charAt(0);
         if (ip == 'X' || ip == 'O' || ip == 'x' || ip == 'o') {
-            System.out.println("You chose " + ip);
+            System.out.println("You chose " + ip+" \n");
 
         } else {
             System.out.println("You chose wrong one!!!");
             input();
         }
     }
+    public static void showboard()
+    {
+        System.out.println("\n Initially the board is empty \n");
+        System.out.println("|-----------|");
+        System.out.println("| " + b[0] + " | "+ b[1] + " | " + b[2]+ " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + b[3] + " | "+ b[4] + " | " + b[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + b[6] + " | " + b[7] + " | " + b[8] + " |");
+        System.out.println("|-----------|");
+    }
+
     public static void main(String[] args) {
         initializeboard();
         input();
+        showboard();
 
     }
 
