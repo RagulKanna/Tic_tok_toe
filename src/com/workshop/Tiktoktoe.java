a2345978945b537c1c594310f145fea307456031
RagulKanna <ragulragi619@gmail.com>
2021-08-30 11:02:44 +0530

    Added:To make choice to firstplay

--- src/com/workshop/Tiktoktoe.java
+++ src/com/workshop/Tiktoktoe.java
@@ -1,10 +1,11 @@
 package com.workshop;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Tiktoktoe {
-    public static int box;
-    public static char ip,ipc;
+    public static int box,choice,move,cplay;
+    public static char Playerchoice,computerchoice;
     public static char[] b=new char[10];
     public static Scanner sc=new Scanner(System.in);
     public static void initializeboard(){
@@ -21,23 +22,23 @@ public class Tiktoktoe {
     }
     public static void input() {
         System.out.println("\nEnter the 'X' or 'O' what you needed:");
-        ip = sc.next().charAt(0);
-        if (ip == 'X' || ip == 'O' || ip == 'x' || ip == 'o')
+        Playerchoice = sc.next().charAt(0);
+        if (Playerchoice == 'X' || Playerchoice == 'O' || Playerchoice == 'x' || Playerchoice == 'o')
         System.out.println("your choice is allocated!!! \n");
             else {
             System.out.println("You chose wrong one!!!");
             input();
         }
-        if(ip == 'x' )
-            ipc= 'o';
-        else if(ip == 'X')
-            ipc = 'O';
-        else if(ip == 'o' )
-            ipc= 'x';
+        if(Playerchoice == 'x' )
+            computerchoice= 'o';
+        else if(Playerchoice == 'X')
+            computerchoice = 'O';
+        else if(Playerchoice == 'o' )
+            computerchoice= 'x';
         else
-            ipc = 'X';
-        System.out.println("You chose " + ip+" \n");
-        System.out.println("Another player chose " + ipc+" \n");
+            computerchoice = 'X';
+        System.out.println("You chose " + Playerchoice+" \n");
+        System.out.println("Another player chose " + computerchoice+" \n");
     }
     public static void showboard()
     {
@@ -53,13 +54,33 @@ public class Tiktoktoe {
 
     public static void selectbox()
     {
-        System.out.println("Select box you need 1 to 9:");
-        box=sc.nextInt();
-        checkbox();
-        showboard();
+        choicetoplay();
+        for(int i=0;i<9;i++) {
+
+            if (choice == 1)
+                move = i % 2;
+            else
+                move = (i + 1) % 2;
+
+            switch (move) {
+                case 0: {
+                    System.out.println("Select box you need 1 to 9:");
+                    box = sc.nextInt();
+                    checkboxPlayer();
+                    showboard();
+                }
+                break;
+                case 1: {
+                    cplay = (int) (Math.floor(Math.random() * 9 % 10) + 1);
+                    box = cplay;
+                    checkboxcomputer();
+                    showboard();
+                } break;
+            }
+        }
     }
 
-    public static void checkbox(){
+    public static void checkboxPlayer(){
         if (box >9 || box <1 ) {
             System.out.println("you chose wrong box!!!");
             selectbox();
@@ -69,13 +90,38 @@ public class Tiktoktoe {
             selectbox();
         }
         else
-            b[box]=ip;
+            b[box]=Playerchoice;
     }
+    public static void checkboxcomputer(){
+        if (box >9 || box <1 ) {
+            System.out.println("you chose wrong box!!!");
+            selectbox();
+        }
+        else if(b[box] !=' ') {
+            System.out.println("The box is Occupied already");
+            selectbox();
+        }
+        else
+            b[box]=computerchoice;
+        showboard();
+    }
+
+    public static void choicetoplay()
+    {
+        choice=(int)(Math.random() * 2) ;
+        if(choice == 1)
+            System.out.println("player 1 is going to play first!!!");
+        else
+            System.out.println("Computer is going to play first");
+
+    }
+
     public static void main(String[] args) {
         initializeboard();
         input();
         showboard();
         selectbox();
+
     }
 
 }
