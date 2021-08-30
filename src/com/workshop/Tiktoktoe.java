2ba2b4a91127d1214b636ecce66f941834b1a26c
RagulKanna <ragulragi619@gmail.com>
2021-08-30 09:34:22 +0530

    Added:To initialize board

--- src/com/workshop/Tiktoktoe.java
+++ src/com/workshop/Tiktoktoe.java
@@ -3,7 +3,7 @@ package com.workshop;
 import java.util.Scanner;
 
 public class Tiktoktoe {
-    public static char ip;
+
     public static Scanner sc=new Scanner(System.in);
     public static void initializeboard(){
         char[] b=new char[10];
@@ -17,23 +17,12 @@ public class Tiktoktoe {
         b[7]=' ';
         b[8]=' ';
         b[9]=' ';
-    }
-    public static void input()
-    {
-    System.out.println("\nEnter the 'X' or 'O' what you needed:");
-    ip=sc.next().charAt(0);
-    if(ip == 'X' || ip =='O' || ip=='x' || ip=='o') {
-        System.out.println("You chose " + ip);
 
-      }
-    else {
-        System.out.println("You chose wrong one!!!");
-        input();
     }
-    }
+
     public static void main(String[] args) {
         initializeboard();
-        input();
+
     }
 
 }
