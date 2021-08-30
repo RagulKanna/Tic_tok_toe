8bdb71ed3e228ea45f72661bf5510156ac702d4c
RagulKanna <ragulragi619@gmail.com>
2021-08-30 11:45:15 +0530

    Added:To check winner

--- src/com/workshop/Tiktoktoe.java
+++ src/com/workshop/Tiktoktoe.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.util.Scanner;
 
 public class Tiktoktoe {
-    public static int box,choice,move,cplay;
+    public static int box,choice,move,cplay,i;
     public static char Playerchoice,computerchoice;
     public static char[] b=new char[10];
     public static Scanner sc=new Scanner(System.in);
@@ -40,7 +40,7 @@ public class Tiktoktoe {
         System.out.println("You chose " + Playerchoice+" \n");
         System.out.println("Another player chose " + computerchoice+" \n");
     }
-    public static void showboard()
+    public static void showboardinitially()
     {
         System.out.println("\n Initially the board is empty \n");
         System.out.println("|-----------|");
@@ -51,11 +51,21 @@ public class Tiktoktoe {
         System.out.println("| " + b[7] + " | " + b[8] + " | " + b[9] + " |");
         System.out.println("|-----------|");
     }
-
+    public static void showboard()
+    {
+        System.out.println("\n \n");
+        System.out.println("|-----------|");
+        System.out.println("| " + b[1] + " | "+ b[2] + " | " + b[3]+ " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + b[4] + " | "+ b[5] + " | " + b[6] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + b[7] + " | " + b[8] + " | " + b[9] + " |");
+        System.out.println("|-----------|");
+    }
     public static void selectbox()
     {
-        choicetoplay();
-        for(int i=0;i<9;i++) {
+
+        for( i=0;i<9;i++) {
 
             if (choice == 1)
                 move = i % 2;
@@ -68,6 +78,7 @@ public class Tiktoktoe {
                     box = sc.nextInt();
                     checkboxPlayer();
                     showboard();
+                    Checkwinner();
                 }
                 break;
                 case 1: {
@@ -75,6 +86,7 @@ public class Tiktoktoe {
                     box = cplay;
                     checkboxcomputer();
                     showboard();
+                    Checkwinner();
                 } break;
             }
         }
@@ -96,10 +108,12 @@ public class Tiktoktoe {
         if (box >9 || box <1 ) {
             System.out.println("you chose wrong box!!!");
             selectbox();
+            i++;
         }
         else if(b[box] !=' ') {
             System.out.println("The box is Occupied already");
             selectbox();
+            i++;
         }
         else
             b[box]=computerchoice;
@@ -115,12 +129,106 @@ public class Tiktoktoe {
             System.out.println("Computer is going to play first");
 
     }
+    public static void Checkwinner() {
+        for (int j = 1; j <=8; j++) {
+
+
+            switch (j) {
+                case 1: {
+                    if (b[1]== Playerchoice && b[2]== Playerchoice && b[3] == Playerchoice) {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    } else if (b[1] == computerchoice && b[2] == computerchoice && b[3] == computerchoice) {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+
+                }break;
+                case 2: {
+                    if (b[7] == Playerchoice && b[8] == Playerchoice && b[9] == Playerchoice) {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    }
+                else if (b[7] == computerchoice && b[8] == computerchoice && b[9] == computerchoice) {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+                }break;
+                case 3: {
+                    if (b[4] == Playerchoice && b[5] == Playerchoice && b[6] == Playerchoice) {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    }
+                else if (b[4] == computerchoice && b[5] == computerchoice && b[6] == computerchoice) {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+                }break;
+                case 4:
+                {
+                    if (b[3] == Playerchoice && b[6] == Playerchoice && b[9] == Playerchoice)
+                    {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    }
+                else if (b[3] == computerchoice && b[6] == computerchoice && b[9] == computerchoice)
+                {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+
+                }break;
+                case 5: {
+                    if (b[1] == Playerchoice && b[4]== Playerchoice && b[7] == Playerchoice) {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    }
+                else if (b[1] == computerchoice && b[4] == computerchoice && b[7] == computerchoice) {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+                }break;
+                case 6: {
+                    if (b[2] == Playerchoice && b[5] == Playerchoice && b[8] == Playerchoice) {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    }
+                    else if (b[2] == computerchoice && b[5] == computerchoice && b[8] == computerchoice) {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+                }break;
+                case 7: {
+                    if (b[1] == Playerchoice && b[5] == Playerchoice && b[9] == Playerchoice) {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    }
+                    else if (b[1] == computerchoice && b[5] == computerchoice && b[9] == computerchoice) {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+                }break;
+                case 8: {
+                    if (b[3] == Playerchoice && b[5] == Playerchoice && b[7] == Playerchoice) {
+                        System.out.println("Player 1 won!!!!");
+                        i = 10;
+                    }
+                    else if (b[3] == computerchoice && b[5] == computerchoice && b[7] == computerchoice) {
+                        System.out.println("Computer Player won!!!!");
+                        i = 10;
+                    }
+                }break;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         initializeboard();
         input();
-        showboard();
+        showboardinitially();
+        choicetoplay();
         selectbox();
+
 
     }
 
